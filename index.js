c69b63572ed6c583168776ccba5b69ec62601141
Rachelle Dupuis <Rachelle.Dupuis@softwire.com>
2022-04-20 14:39:22 +0100

    Implement rules for multiples of 7

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-for (let i = 1; i <= 100; i++ ) {
+for (let i = 1; i <= 200; i++ ) {
     let fizzBuzzArray = [];
     if (i % 3 === 0) {
         fizzBuzzArray.push('Fizz');
     }
     if (i % 5 === 0) {
-        fizzBuzzArray.push('Buzz')
+        fizzBuzzArray.push('Buzz');
+    }
+    if (i % 7 === 0) {
+        fizzBuzzArray.push('Bang');
     }
     if (fizzBuzzArray.length === 0) {
         fizzBuzzArray.push(i);
